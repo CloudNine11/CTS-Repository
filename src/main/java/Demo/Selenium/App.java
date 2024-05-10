5db490295a57d6a323b3e1eb4acfe9ca2f543413
KausaniDas01 <102321348+CloudNine11@users.noreply.github.com>
2024-05-10 16:43:02 +0530

    Add files via upload

---
+++ src/main/java/Demo/Selenium/App.java
@@ -0,0 +1,17 @@
+package Demo.Selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        WebDriverManager.chromedriver().setup();
+        
+        ChromeDriver driver=new ChromeDriver();
+    }
+}
